343198c7280306c33f2697148fa06db212a30c48
David L. Inman <davidlevere@gmail.com>
2022-07-17 16:15:04 -0400

    display inc prog

--- src/evaling/display.rs
+++ src/evaling/display.rs
@@ -8,6 +8,10 @@ pub fn print_data(data : &Data, context : &Context) -> Result<String, RuntimeErr
         Data::Number(n) => Ok(n.to_string()),
         Data::String(s) => Ok(format!("\"{}\"", s)),
         Data::Symbol(s) => Ok(s.into()),
+        Data::Variable(var) => {
+            let data = context.lookup(var)?;
+            print_data(&data, context)
+        },
         _ => Ok("err".into()),
     }
 }
