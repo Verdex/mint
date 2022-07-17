dd92ed0a59ef4449d32ef2a95644dfae001c2287
David L. Inman <davidlevere@gmail.com>
2022-07-17 16:15:19 -0400

    inc prog for repl

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,18 @@ mod evaling;
 fn main() {
     use std::io::{stdout, stdin, Write};
 
+    let mut c = evaling::context::Context::new();
+
     loop {
         print!("> ");
-        stdout().flush();
+        stdout().flush().unwrap();
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
         let tokens = parsing::tokenizer::tokenize(&input).unwrap();
         let top = parsing::parser::parse(&tokens).unwrap();
-        let mut c = evaling::context::Context::new();
         let data = evaling::evaler::eval(top, &mut c).unwrap();
 
-        println!("{}", evaling::display::print_data(&data, &c));
+        println!("{}", evaling::display::print_data(&data, &c).unwrap());
     } 
 }
