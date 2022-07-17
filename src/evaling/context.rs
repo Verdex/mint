a1d687b8176058a4a4d5d55ca5c026612e66cab7
David L. Inman <davidlevere@gmail.com>
2022-07-17 15:55:29 -0400

    bound variables and lookup for context

--- src/evaling/context.rs
+++ src/evaling/context.rs
@@ -1,10 +1,22 @@
 
-pub struct Context {
+use std::collections::HashMap;
 
+use crate::ast::Data;
+
+pub struct Context {
+    bound_variables : HashMap<String, Data>,
 }
 
 impl Context {
     pub fn new() -> Self {
-        Context { }
+        Context { bound_variables : HashMap::new() }
     }
+
+    pub fn lookup(&self, name : &str) -> Option<Data> {
+        match self.bound_variables.get(name) {
+            Some(data) => Some(data.clone()),
+            None => None,
+        }
+    }
+
 }
