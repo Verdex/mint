679ffa23796b8bf828230f993b8a5dd05f43f14f
David L. Inman <davidlevere@gmail.com>
2022-07-17 15:05:11 -0400

    display module for data returned by eval

---
+++ src/evaling/display.rs
@@ -0,0 +1,7 @@
+
+use crate::ast::Data;
+use super::context::Context;
+
+pub fn print_data(data : &Data, context : &Context) -> String {
+    "err".into()
+}
--- src/evaling/mod.rs
+++ src/evaling/mod.rs
@@ -1,3 +1,4 @@
 
 pub mod context;
 pub mod evaler;
+pub mod display;
