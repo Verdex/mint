9653a066c1cc0655a6c91928df3c7c3f63f3cbf9
David L. Inman <davidlevere@gmail.com>
2022-07-17 15:59:40 -0400

    print data now returns result

--- src/evaling/display.rs
+++ src/evaling/display.rs
@@ -1,10 +1,13 @@
 
 use crate::ast::Data;
 use super::context::Context;
+use super::error::RuntimeError;
 
-pub fn print_data(data : &Data, context : &Context) -> String {
+pub fn print_data(data : &Data, context : &Context) -> Result<String, RuntimeError> {
     match data {
-        Data::Number(n) => n.to_string(),
-        _ => "err".into(),
+        Data::Number(n) => Ok(n.to_string()),
+        Data::String(s) => Ok(format!("\"{}\"", s)),
+        Data::Symbol(s) => Ok(s.into()),
+        _ => Ok("err".into()),
     }
 }
