a1485dd2873d6e341041b15441aa3c428cb12665
David L. Inman <davidlevere@gmail.com>
2022-07-17 15:54:43 -0400

    variable not found error

--- src/evaling/error.rs
+++ src/evaling/error.rs
@@ -2,16 +2,13 @@
 
 #[derive(Debug)]
 pub enum RuntimeError {
-
+    VariableNotFound(String),
 }
 
 impl std::fmt::Display for RuntimeError {
     fn fmt(&self, f : &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            MatchError::Error(x) => write!(f, "encountered error at:  {}", x),
-            MatchError::ErrorEndOfFile => write!(f, "encountered end of file error"),
-            MatchError::Fatal(x) => write!(f, "encountered fatal error at:  {}", x),
-            MatchError::FatalEndOfFile => write!(f, "encountered fatal end of file" ),
+            RuntimeError::VariableNotFound(s) => write!(f, "could not find variable:  {}", s),
         }
     }
 }
