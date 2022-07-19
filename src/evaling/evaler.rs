d48d5a773dd2c932b71d9c119ccdf7c6996dc4f4
David L. Inman <davidlevere@gmail.com>
2022-07-18 22:03:07 -0400

    pattern matcher module

--- src/evaling/evaler.rs
+++ src/evaling/evaler.rs
@@ -7,6 +7,7 @@ use crate::ast::{ Top
 use super::context::Context;
 
 pub fn eval( input : Top, context : &mut Context ) -> Result<Data, String> {
+
     // TODO resolve lets
     match input.expr {
         Some(Expr::Data(data)) => Ok(data),
--- src/evaling/mod.rs
+++ src/evaling/mod.rs
@@ -1,5 +1,7 @@
 
 pub mod context;
+
+mod pattern_matcher;
 pub mod evaler;
 pub mod display;
 pub mod error;
