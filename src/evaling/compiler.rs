4ab9cbb2eb6fb776873e31685d6e3ac5935a5dac
David L. Inman <davidlevere@gmail.com>
2022-08-03 21:51:58 -0400

    compiler and vm empty files

---
+++ src/evaling/compiler.rs
@@ -0,0 +1,12 @@
+
+/*use crate::ast::{ Top
+                , Data
+                , Expr
+                };
+
+use super::context::{Context, RuntimeData};
+
+pub fn compile(t : Top) -> RuntimeData {
+
+}
+*/
