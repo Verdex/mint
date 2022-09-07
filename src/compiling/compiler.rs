420556c4e86c42946e790a3580001a77774db32a
David L. Inman <davidlevere@gmail.com>
2022-09-06 22:34:22 -0400

    notes

--- src/compiling/compiler.rs
+++ src/compiling/compiler.rs
@@ -1,13 +1,21 @@
 
+use std::collections::HashMap;
+
 use purple::data::*;
+use crate::runtime::*;
 
 use super::error::CompileError;
 
-// in:
-// list of existing data
-// top
-// out:
-// more data
-// 
 
-//pub fn compile(top : &Top) -> Result<HashMap<Func, Vec<Instr< , >>>, CompileError>
+type I = Instr<RuntimeData, HashMap<HeapAddress, RuntimeData>>;
+type P = HashMap<Func, Vec<I>>; 
+
+// NOTE:  The returned Vec<I> is the entry point for this compile.
+
+// The input program will be any pre-existing functions that have already
+// been defined on previous repl executions.
+
+// TODO need to be able to call pre-existing function by name not Func
+// TODO need to be able to use pre-existing data
+
+pub fn compile(top : &Top, program : &mut P) -> Result<Vec<I>, CompileError>
