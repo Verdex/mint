e91c3b696a176c2934debc79b463f4d0cfd718c2
David L. Inman <davidlevere@gmail.com>
2022-09-20 21:18:04 -0400

    inc prog for eval

--- src/evaling/evaler.rs
+++ src/evaling/evaler.rs
@@ -8,7 +8,7 @@ use crate::compiling::compiler;
 use super::data::Context;
 use super::error::RuntimeError;
 use super::pattern_matcher::*;
-
+use super::display::*;
 
 pub fn eval( input : Top, context : &mut Context ) -> Result<Option<String>, Box<dyn std::error::Error>> {
     for l in input.lets {
@@ -46,5 +46,19 @@ pub fn eval( input : Top, context : &mut Context ) -> Result<Option<String>, Box
             },
         }
     }
-    Ok(None)
+
+    if input.expr.is_none() {
+        Ok(None)
+    }
+    else {
+        let program = compiler::compile(&input.expr.unwrap(), &context.address_map, &mut context.functions)?;
+        // Note:  We can leave functions alone after we're done because the next eval will flush Func(0)
+        context.functions.insert(Func(0), program); 
+        let result = purple::run(&context.functions, &mut context.heap)?;
+        match result {
+            Some(Data::Value(v)) => Ok(Some(print_data(&v))),
+            Some(Data::Func(f)) => Ok(Some(print_data(&RuntimeData::Function(f)))),
+            None => Ok(None),
+        }
+    }
 }
