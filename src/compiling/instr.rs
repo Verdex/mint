bece1341490af09496fa60c53216da848c99080f
David L. Inman <davidlevere@gmail.com>
2022-10-09 00:55:57 -0400

    insert_into_heap impl

--- src/compiling/instr.rs
+++ src/compiling/instr.rs
@@ -15,3 +15,20 @@ pub fn load_from_heap(address : HeapAddress, sym : Symbol) -> Instr<RuntimeData,
                 }
             }))
 }
+
+pub fn insert_into_heap(symbol_to_insert : Symbol, return_symbol_for_address : Symbol) -> Instr<RuntimeData, Heap> {
+    Instr::<RuntimeData, Heap>::LoadFromSysCall(return_symbol_for_address, Box::new(
+        move |locals, heap| {
+            match locals.get(&symbol_to_insert)? {
+                Data::Value(value) => {
+                    let address = heap.insert_new(value);
+                    Ok(Data::Value(RuntimeData::Address(address)))
+                },
+                Data::Func(f) => {
+                    let address = heap.insert_new(RuntimeData::Function(f));
+                    Ok(Data::Value(RuntimeData::Address(address)))
+                },
+            }
+        }
+    ))
+}
