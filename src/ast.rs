758b0923ce87df0bb92c6c018e0af2994d697a97
David L. Inman <davidlevere@gmail.com>
2022-10-12 23:39:47 -0400

    function for getting bound variables out of a Top

--- src/ast.rs
+++ src/ast.rs
@@ -81,3 +81,73 @@ pub struct Top {
     pub lets : Vec<Let>,
     pub expr : Option<Expr>,
 }
+
+use denest::Linearizable;
+
+impl<'a> Linearizable<'a> for Pat {
+    
+    fn l_next(&'a self) -> Vec<&'a Self> {
+        use crate::ast::Pat::*;
+
+        match self {
+            Wild => vec![],
+            Number(_) => vec![],
+            String(_) => vec![],
+            Symbol(_) => vec![],
+            List(ps, Some(mp)) => ps.iter().chain(std::iter::once(&**mp)).collect(),
+            List(ps, None) => ps.iter().collect(),
+            Tuple(ps) => ps.iter().collect(),
+            Variable(_) => vec![],
+            At(_, p) => vec![ p ],
+        }
+    }
+}
+
+pub fn bound_variables<'a>( input : &'a Top ) -> Vec<&'a str> {
+    input.lets.iter().map(|x| &x.pattern)
+                     .flat_map(|x| x.to_lax())
+                     .filter(|p| matches!(p, Pat::Variable(_) | Pat::At(_, _)))
+                     .map(|p| match p {
+                        Pat::Variable(x) => x.as_str(),
+                        Pat::At(x, _) => x.as_str(),
+                        _ => unreachable!(), 
+                     }).collect()
+}
+
+#[cfg(test)]
+mod test { 
+    use super::*;
+
+    #[test]
+    fn bound_variables_should_return_all_variables_in_top_scope() {
+        let tokens = crate::parsing::tokenizer::tokenize(
+            "let X = 5;
+             let Y = 6;
+             let Z @ _  = 7;
+             let { W } = 8;
+             let H @ { N } = 9;
+             ").unwrap();
+        let top = crate::parsing::parser::parse(&tokens).unwrap();
+
+        let output = bound_variables(&top);
+
+        assert_eq!( output, vec!["X", "Y", "Z", "W", "H", "N"] );
+    }
+
+    #[test]
+    fn bound_variables_should_return_no_inner_scope_variables() {
+        let tokens = crate::parsing::tokenizer::tokenize(
+            "let X = fun(A, B, C) { let D = 8; };
+             let Y = 6;
+             let Z @ _  = 7;
+             let { W } = 8;
+             let H @ { N } = 9;
+             ").unwrap();
+        let top = crate::parsing::parser::parse(&tokens).unwrap();
+
+        let output = bound_variables(&top);
+
+        assert_eq!( output, vec!["X", "Y", "Z", "W", "H", "N"] );
+    }
+
+}
